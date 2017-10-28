cddad504f34decf8760b416ffe69ec8868febf14
Gabe Bravo <gabecbravo@gmail.com>
2017-10-28 09:33:45 -0400

    Made config changes to try and re-deploy Hapi React app.

--- server/database.js
+++ server/database.js
@@ -1,10 +1,11 @@
-var Mongoose = require('mongoose');
+const Mongoose = require('mongoose');
+const mLabDB = process.env.MONGODB_URI;
 //load database
 Mongoose.Promise = global.Promise;
-Mongoose.connect('mongodb://localhost/todos', {
+Mongoose.connect(mLabDB, {
     useMongoClient: true,
 });
-var db = Mongoose.connection;
+const db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
     console.log('Connection with database succeeded.');
--- server/index.js
+++ server/index.js
@@ -1,14 +1,50 @@
+// import DB path for DB connect
 const db = require('./database').db;
+// import all dependencies
+const Path = require('path');
 const Hapi = require('hapi');
-const server = new Hapi.Server();
-server.connection({ port: 3001, host: 'localhost' });
+const Inert = require('inert');
+
+// create server
+const server = new Hapi.Server({
+  connections: {
+      routes: {
+          files: {
+              relativeTo: Path.join(__dirname, '../client/build')
+          }
+      }
+  }
+});
+
+// invoke server
+server.connection({ port: process.env.PORT });
+// register inert middleware
+server.register(Inert, () => {});
+
+// path to root index.html file
+// path: '/{path*}'
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: {
+      file: 'index.html'
+  }
+});
 
 server.route({
     method: 'GET',
-    path: '/api',
-    handler: function (request, reply) {
-        reply({'message': 'Hello, API!'});
-    }
+    path: '/static/css/main.286e6ea6.css',
+    handler: {
+        file: 'static/css/main.286e6ea6.css'
+    }  
+});
+
+server.route({
+    method: 'GET',
+    path: '/static/js/main.8d4b333d.js',
+    handler: {
+        file: 'static/js/main.8d4b333d.js'
+    }  
 });
 
 const routes = require('./routes');
@@ -21,3 +57,26 @@ server.start((err) => {
     }
     console.log(`Server running at: ${server.info.uri}`);
 });
+
+// const Hapi = require('hapi');
+// const server = new Hapi.Server();
+// server.connection({ port: 3001, host: 'localhost' });
+
+// server.route({
+//     method: 'GET',
+//     path: '/api',
+//     handler: function (request, reply) {
+//         reply({'message': 'Hello, API!'});
+//     }
+// });
+
+// const routes = require('./routes');
+// server.route(routes);
+
+// server.start((err) => {
+
+//     if (err) {
+//         throw err;
+//     }
+//     console.log(`Server running at: ${server.info.uri}`);
+// });
